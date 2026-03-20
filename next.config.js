541faec456caaec7c2f5243b6fb2f3ac01038e5f
Edukron <37039644+Analytics6@users.noreply.github.com>
2026-03-20 18:15:37 +0000

    version2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
     domains: ['images.unsplash.com', 'via.placeholder.com'],
   },
-  basePath: '/Interviewquestions',
-  assetPrefix: '/Interviewquestions/',
 }
 
 module.exports = nextConfig
